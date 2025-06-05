4dd5482e367d6d081cab624a2bdbf2bac456ff7e
Isaev_Sergey <serzi6551@gmail.com>
2025-06-05 07:19:21 +0300

    My portfolio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    let burger = $('#burger');
+    let close = $('#close');
+    let nav = $('#menu');
+    burger.on('click',function(){
+        nav.show('slow');
+    });
+    close.on('click',function(event){
+        console.log($(event).target)
+        nav.hide('slow');
+    });
+    nav.on('click',function(){
+        nav.hide('slow');
+    })
+})
